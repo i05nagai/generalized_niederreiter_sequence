835239c7bd6e2609f5f371a5eaa6c9f7f842d58d
i05nagai <i05nagai@gmail.com>
2017-04-27 22:24:44 +0900

    Add cmake files

---
+++ cmake/gf.cmake
@@ -0,0 +1,8 @@
+LIST (APPEND gf_SOURCES
+    gf/Polynomial.cc
+	gf/IrreduciblePolynomial.cc
+)
+LIST (APPEND gf_HEADERS
+    gf/Polynomial.h
+	gf/IrreduciblePolynomial.h
+)
---
+++ cmake/gf_test.cmake
@@ -0,0 +1,7 @@
+LIST (APPEND gf_test_SOURCES
+	gf/main.cc
+    gf/PolynomialTest.cc
+	gf/IrreduciblePolynomialTest.cc
+	gf/galois_field_operator_test.cc
+	gf/util_test.cc
+)
