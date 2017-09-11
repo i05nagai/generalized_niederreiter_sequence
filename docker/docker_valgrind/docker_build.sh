62cab12859bdb3db8563c60282a09ae33231d1da
i05nagai <i05nagai@gmail.com>
2017-09-11 18:00:38 +0900

    Fix docker files for valgrind

--- docker/docker_valgrind/docker_build.sh
+++ docker/docker_valgrind/docker_build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cp ../../ci/travis_cmake_setup.sh travis_cmake_setup.sh
+cp ../../scripts/travis_cmake_setup.sh travis_cmake_setup.sh
+chmod 755 travis_cmake_setup.sh
 docker build . -t gns/valgrind:latest
